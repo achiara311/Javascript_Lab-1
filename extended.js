0e3315546776506a9fc8e5dbb6ace8f50a378427
Anthony Chiara <achiara12@gmail.com>
2020-07-28 09:42:09 -0400

    weeks problem

--- extended.js
+++ extended.js
@@ -5,13 +5,18 @@ let pay= 0;
 if(hours <= 40){
     pay = hours * wages;
     console.log(`Current Pay: $${pay}`);
+    let weeks= Math.ceil(10000000/regularHours);
+    console.log(`With wage=${wage} and hours=${hours} it would take ${weeks} weeks.`)
 }
 else{
     let regularHours = 40 * wages; 
-    let overtime = (regularHours - 40) * 1.50;
+    let overtime = (regularHours - 40) * (wage*1.50);
     pay = regularHours + overtime;
     console.log(`Overtime: $${pay}`);
+    let weeks= Math.ceil(10000000/regularHours);
+    console.log(`With wage=${wage} and hours=${hours} it would take ${weeks} weeks.`)
 }
 
 //Weeks
-
+let weeks=10000000/regularHours;
+console.log(`With wage=${wage} and hours=${hours} it would take ${weeks} weeks.`)
--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-    let name="Anthony";
-    let age=27;
-    let birthday="March 11th";
+    "use strict";
+    
+    let name = "Anthony";
+    let age = 27;
+    let birthday = "March 11th";
     let detroitGC = true;
-    let lifeEvents=[
+    let lifeEvents = [
     "I was born in South Lyon, MI",
     "I went to Michigan State","I went to London with my high school marching band for New Year's Day Parade",
     "I'm a graduate of the C# bootcamp and current student of the front end bootcamp."];
 
-    if(detroitGC === true){
+    if(detroitGC){
         console.log(`My name is ${name} and I am a student at Grand Circus in Detroit, Michigan.I am currently ${age} years old and my birthday is on ${birthday}.`);
      }
    else{
@@ -16,12 +18,14 @@
        }
 
   for(let i= 0;i<=lifeEvents.length-1;i++){
+    //   "i" can be used to access the elements in the array
     console.log(lifeEvents[i]);
 }
 // console.log("4.");
  let counter = 0;
  while(true){
     let randomNumber = Math.floor(Math.random() * 10) + 1;
+    // OR let randomNumber = Math.floor(Math.random() * 10 +1);
     if(randomNumber !== 5){
     counter++;
     console.log(`${randomNumber} !== 5`);
